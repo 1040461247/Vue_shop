9a189bd7ecbc5eefbe604feed6bc0058183ed9e5
田若茜 <1040461147@qq.com>
2020-07-02 09:37:37 +0800

    更改了接口url

--- src/network/request.js
+++ src/network/request.js
@@ -7,7 +7,7 @@ import 'nprogress/nprogress.css'
 export function request (config) {
   // 创建axios实例
   const instance = axios.create({
-    baseURL: 'https://www.liulongbin.top:8888/api/private/v1/',
+    baseURL: 'http://timemeetyou.com:8889/api/private/v1/',
     timeout: 5000
   })
 
